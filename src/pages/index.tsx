0b9e43cd67cc7166312f382bcb3e1e305d46a7e6
jstevenson02 <jacobryanstevenson@gmail.com>
2023-03-30 17:01:30 -0500

    Posting, Loading states and Toasts!

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,16 @@
+import { useState } from "react";
 import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 import { type NextPage } from "next";
+import { LoadingPage, LoadingSpinner } from "~/components/loading";
 import Head from "next/head";
+import Image from "next/image";
+
+import dayjs from "dayjs";
+import { toast } from "react-hot-toast";
+import relativeTime from "dayjs/plugin/relativeTime";
 
 import type { RouterOutputs } from "~/utils/api";
 import { api } from "~/utils/api";
-
-import dayjs from "dayjs";
-import relativeTime from "dayjs/plugin/relativeTime";
-import Image from "next/image";
-import { LoadingPage } from "~/components/loading";
-import { useState } from "react";
 
 dayjs.extend(relativeTime);
 
@@ -24,6 +25,16 @@ const CreatePostWizard = () => {
     onSuccess: () => {
       setInput("");
       void ctx.posts.getAll.invalidate();
+    },
+
+    onError: (e) => {
+      const errorMessage = e.data?.zodError?.fieldErrors.content;
+
+      if (errorMessage && errorMessage[0]) {
+        toast.error(errorMessage[0]);
+      } else {
+        toast.error("Failed to post! Please try again later.");
+      }
     },
   });
 
@@ -43,9 +54,24 @@ const CreatePostWizard = () => {
         placeholder="Type some emojis..."
         value={input}
         onChange={(e) => setInput(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            e.preventDefault();
+            if (input !== "") {
+              mutate({ content: input });
+            }
+          }
+        }}
         disabled={isPosting}
       />
-      <button onClick={() => mutate({ content: input })}>Post</button>
+      {input !== "" && !isPosting && (
+        <button onClick={() => mutate({ content: input })}>Post</button>
+      )}
+      {isPosting && (
+        <div className="flex items-center justify-center">
+          <LoadingSpinner size={20} />
+        </div>
+      )}
     </div>
   );
 };
